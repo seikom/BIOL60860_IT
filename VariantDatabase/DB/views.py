b73ad0a33db345be44f638967fa3b53c2eb5232d
Sam Rowlston <samuel.rowlston@postgrad.manchester.ac.uk>
2021-02-17 14:16:25 +0000

    Added initial view

--- VariantDatabase/DB/views.py
+++ VariantDatabase/DB/views.py
@@ -1,3 +1,26 @@
+from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
+from .models import Testvariant
 
 # Create your views here.
+
+def Test(request):
+
+    """displays test result
+    """
+
+    test = Testvariant.objects.all()
+
+    return (request, 'DB/main.html', {test : 'test'})
+
+
+
+
+def Get_variant(request):
+
+    """Gets a specific variants according to the ID
+    """
+
+    return None
+
+
