cc0f3abc33573b5c0814765b16f12d0377dd4219
Seiko Makino <seikomakino@gmail.com>
2021-02-17 15:07:18 +0000

    testvariant model fields updated

--- VariantDatabase/DB/models.py
+++ VariantDatabase/DB/models.py
@@ -5,16 +5,19 @@ from django.utils import timezone
 # Create your models here.
 class Testvariant(models.Model):
     name = models.CharField(max_length=200)
-    age = models.IntegerField()
-    proband = models.BooleanField()
-    #
-    sequencer = models.CharField()
-    variant_cdna = models.CharField()
-    variant_protein = models.CharField()
-    variant_genome = models.CharField(blank=False)
-    code_pathogenicity = models.IntegerField(blank=True)
+    age = models.IntegerField(max_length=3, )
+    proband = models.BooleanField(blank=True)
+    affected_relatives = models.BooleanField(blank=True)
+    stage = models.IntegerField(choices=(("1","1"),("2","2"),("3","3")))
+    description = models.CharField(max_length=500)
+    sequencer = models.CharField(cohices=(("HiSeq", "HiSeq"), ("MiSeq", "MiSeq")))
+    gene = models.CharField(max_length=10, blank=True)
+    variant_cdna = models.CharField(max_length=200)
+    variant_protein = models.CharField(max_length=200)
+    variant_genome = models.CharField(blank=False, max_length=200)
+    code_pathogenicity = models.CharField(blank=True, choices=(("1","1"),("2","2"),("3","3"),("4","4"),("5","5")), max_length=1)
     codes_evidence = models.CharField(max_length=200, blank=True)
     uploaded_time = models.DateTimeField(blank=True, null=True)
-
+#todo: split the table into separate tables
     def __str__(self):
         return self.name
