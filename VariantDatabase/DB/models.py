7aae9a9a92061899ff60101294fb5808f359bf56
Seiko Makino <seikomakino@gmail.com>
2021-02-17 15:19:21 +0000

    testvariant model fields updated

--- VariantDatabase/DB/models.py
+++ VariantDatabase/DB/models.py
@@ -7,7 +7,7 @@ class Testvariant(models.Model):
     name = models.CharField(max_length=200)
     age = models.IntegerField()
     proband = models.BooleanField(blank=True)
-    affected_relatives = models.BooleanField(blank=True)
+    affected_relatives = models.BooleanField()
     stage = models.IntegerField(choices=(("1","1"),("2","2"),("3","3")))
     description = models.CharField(max_length=500)
     sequencer = models.CharField(choices=(("HiSeq", "HiSeq"), ("MiSeq", "MiSeq")), max_length=20)
