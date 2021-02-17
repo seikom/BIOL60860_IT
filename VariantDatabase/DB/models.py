bf2528c4b1f8c502e2f796e816d5cf4048e5a350
Seiko Makino <seikomakino@gmail.com>
2021-02-17 14:38:55 +0000

    testvariant model some fields added

--- VariantDatabase/DB/models.py
+++ VariantDatabase/DB/models.py
@@ -7,6 +7,13 @@ class Testvariant(models.Model):
     name = models.CharField(max_length=200)
     age = models.IntegerField()
     proband = models.BooleanField()
+    #
+    sequencer = models.CharField()
+    variant_cdna = models.CharField()
+    variant_protein = models.CharField()
+    variant_genome = models.CharField(blank=False)
+    code_pathogenicity = models.IntegerField(blank=True)
+    codes_evidence = models.CharField(max_length=200, blank=True)
     uploaded_time = models.DateTimeField(blank=True, null=True)
 
     def __str__(self):
