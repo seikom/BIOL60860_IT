c38d6cb444afe494c10a52aede6f47e1c0ff1c84
Seiko Makino <seikomakino@gmail.com>
2021-02-18 15:24:30 +0000

    admin updated

--- VariantDatabase/DB/admin.py
+++ VariantDatabase/DB/admin.py
@@ -4,4 +4,4 @@ from .models import Patient_data, Variant_data, Test_data, Interpretation_data
 admin.site.register(Patient_data)
 admin.site.register(Variant_data)
 admin.site.register(Test_data)
-admin.wite.register(Interpretaion_data)
+admin.site.register(Interpretaion_data)
